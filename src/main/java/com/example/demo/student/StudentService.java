ecc994485ef77a688e6d66633b1bbda430c57922
imsamuka <imsamuka@gmail.com>
2022-03-28 21:01:57 -0300

    add POST, DELETE and PUT to Student API

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,9 +3,12 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.Transient;
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -20,4 +23,37 @@ public class StudentService {
     public List<Student> getStudents(){
         return studentRepository.findAll();
     }
+
+    public void addNewStudent(Student student) {
+        if (studentRepository.findStudentByEmail(student.getEmail()).isPresent()){
+            throw new IllegalStateException("Email already taken");
+        }
+
+        studentRepository.save(student);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email){
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("Student with id '" + studentId + "' does not exists"));
+
+        if (name != null && name.length() > 0 && !name.equals(student.getName()))
+            student.setName(name);
+
+        if (email != null && email.length() > 0 && !email.equals(student.getEmail())){
+            if (studentRepository.findStudentByEmail(email).isPresent()){
+                throw new IllegalStateException("Email already taken");
+            }
+            student.setEmail(email);
+        }
+    }
+
+    public void removeStudent(Long studentId) {
+        if (!studentRepository.existsById(studentId)){
+            throw new IllegalStateException("Student with id '" + studentId + "' does not exists");
+        }
+
+        studentRepository.deleteById(studentId);
+    }
+
 }
