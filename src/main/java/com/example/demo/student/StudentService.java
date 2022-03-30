83f8e8bd9cc48d8f7f5ef1c72aae35d120aa8b2b
imsamuka <imsamuka@gmail.com>
2022-03-30 12:58:40 -0300

    add java tool version

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,12 +3,8 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.Transient;
 import javax.transaction.Transactional;
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class StudentService {
